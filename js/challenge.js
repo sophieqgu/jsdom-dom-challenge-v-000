b7b38b35ac2629d7fbffebe5fc807d03e8c3a919
Sophie Gu <sophieqgu@gmail.com>
2021-01-05 05:14:52 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let counter = getElementById('counter');
+  let counter = document.getElementById('counter');
   let plus = document.getElementById('plus');
   let minus = document.getElementById('minus');
   let heart = document.getElementById('heart');
@@ -7,23 +7,25 @@ document.addEventListener("DOMContentLoaded", function() {
   let likes = document.querySelector('likes');
 
   let submitButton = document.getElementById('submit');
-  let commentForm = document.getElementById('comment-form');
+  let commentForm = document.getElementById('comment-form')
+
 
   let count = setInterval(() => {
-    counter.innterHTML++
-  }, 1000);
+    counter.innerHTML++
+  }, 1000)
 
   plus.addEventListener('click', () => {
     counter.innerHTML++
-  });
+  })
 
   minus.addEventListener('click', ()=> {
     counter.innerHTML--
-  });
+  })
 
   pause.addEventListener('click', () => {
-    if (pause.innerText === "pause") {
+    if(pause.innerText === "pause"){
       clearInterval(count)
+
       pause.innerHTML = "resume";
       plus.disabled = true;
       minus.disabled = true;
@@ -39,14 +41,34 @@ document.addEventListener("DOMContentLoaded", function() {
       heart.disabled = false;
       submitButton.disabled = false;
     }
-  });
+  })
 
   submitButton.addEventListener("click", (e) => {
     e.preventDefault();
+
     const p = document.createElement('p');
     p.innerText = commentForm.querySelector('input').value;
     document.getElementById('list').appendChild(p);
     commentForm.reset();
+  });
+
+  const likeCount = {}
+  heart.addEventListener("click", (e) => {
+    num = parseInt(counter.innerText);
+
+   likeCount[num] ? likeCount[num]++ : likeCount[num] = 1;
+
+    if (document.getElementById(`${num}`)) {
+      document.getElementById(`${num}`).innerText = `${num} has been liked ${likeCount[num]} times`;
+    }
+    else {
+      let li = document.createElement('li');
+      li.innerText = `${num} has been liked ${likeCount[num]} times`;
+
+      li.setAttribute('id', `${num}`);
+      document.querySelector('ul.likes').appendChild(li);
+    }
+
   });
 
 });
