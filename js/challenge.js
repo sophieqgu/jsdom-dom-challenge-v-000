210de20e0b2d9777168e31f3e7a90ec669385672
Sophie Gu <sophieqgu@gmail.com>
2021-01-05 05:20:54 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -5,27 +5,25 @@ document.addEventListener("DOMContentLoaded", function() {
   let heart = document.getElementById('heart');
   let pause = document.getElementById('pause');
   let likes = document.querySelector('likes');
-
   let submitButton = document.getElementById('submit');
-  let commentForm = document.getElementById('comment-form')
+  let commentForm = document.getElementById('comment-form');
 
 
   let count = setInterval(() => {
     counter.innerHTML++
-  }, 1000)
+  }, 1000);
 
   plus.addEventListener('click', () => {
     counter.innerHTML++
-  })
+  });
 
   minus.addEventListener('click', ()=> {
     counter.innerHTML--
-  })
+  });
 
   pause.addEventListener('click', () => {
     if (pause.innerText === "pause") {
-      clearInterval(count)
-
+      clearInterval(count);
       pause.innerHTML = "resume";
       plus.disabled = true;
       minus.disabled = true;
@@ -35,18 +33,17 @@ document.addEventListener("DOMContentLoaded", function() {
       counter.innerHTML = 0;
       count = setInterval(() => {
         counter.innerHTML++
-      }, 1000)
-      pause.innerHTML = "pause"
+      }, 1000);
+      pause.innerHTML = "pause";
       plus.disabled = false;
       minus.disabled = false;
       heart.disabled = false;
       submitButton.disabled = false;
     }
-  })
+  });
 
   submitButton.addEventListener("click", (e) => {
     e.preventDefault();
-
     const p = document.createElement('p');
     p.innerText = commentForm.querySelector('input').value;
     document.getElementById('list').appendChild(p);
@@ -57,19 +54,15 @@ document.addEventListener("DOMContentLoaded", function() {
   heart.addEventListener("click", (e) => {
     num = parseInt(counter.innerText);
 
-   likeCount[num] ? likeCount[num]++ : likeCount[num] = 1;
+    likeCount[num] ? likeCount[num]++ : likeCount[num] = 1;
 
     if (document.getElementById(`${num}`)) {
       document.getElementById(`${num}`).innerText = `${num} has been liked ${likeCount[num]} times`;
-    }
-    else {
+    } else {
       let li = document.createElement('li');
       li.innerText = `${num} has been liked ${likeCount[num]} times`;
-
       li.setAttribute('id', `${num}`);
       document.querySelector('ul.likes').appendChild(li);
     }
-
   });
-
 });
